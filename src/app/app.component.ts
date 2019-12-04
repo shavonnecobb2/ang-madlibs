d57c038031bb4b0d369166a556f338f5dec5731e
Shavonne Cobb <Shavonne.Cobb@improving.com>
2019-12-04 09:01:12 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  color: string;
+  foodItem: string;
+  petName: string;
+  cityName: string;
+  countryName: string;
+
   title = 'madlibs';
+
+
+  save() {
+    console.log(this.foodItem && this.color && this.petName
+    && this.cityName && this.countryName);
+  }
+
+  reset() {
+    this.foodItem = '';
+    this.color = '';
+    this.petName = '';
+    this.cityName = '';
+    this.countryName = '';
+  }
 }
